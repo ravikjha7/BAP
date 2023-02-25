c6528a3b5f0079c5cd3092049ce511a73d71dd9e
ravikjha7 <ravikjha7@gmail.com>
2023-02-25 18:59:29 +0530

    Get User Updated

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ const auth = require('./../middlewares/auth');
 
 router.route('/verify/:token').get(users.verifyToken);
 router.route('/profile/login').post(users.login);
-router.route('/profile/:email').get(auth.auth, users.getProfile);
-router.route('/profile').post(users.setProfile).put(auth.auth, users.updateProfile);
+router.route('/profile').post(users.setProfile).put(auth.auth, users.updateProfile).get(auth.auth, users.getProfile);
 
 module.exports = router;
